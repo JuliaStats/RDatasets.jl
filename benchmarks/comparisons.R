ad11bbd2dee2c63aae681b33c826944fed927634
John Myles White <jmw@johnmyleswhite.com>
2012-11-27 09:23:14 -0500

    Revised comparison plots

--- benchmarks/comparisons.R
+++ benchmarks/comparisons.R
@@ -3,7 +3,10 @@ df <- read.csv(file.path("results", "performance.tsv"), sep = "\t")
 ggplot(melt(df), aes(x = variable, y = value, fill = variable)) +
   stat_summary(fun.data = "mean_cl_boot", geom = "bar") +
   stat_summary(fun.data = "mean_cl_boot", geom = "errorbar") +
-  coord_flip() +
+  theme(legend.position = "none")
+
+ggplot(melt(df), aes(x = variable, y = value, fill = variable)) +
+  geom_boxplot() +
   theme(legend.position = "none")
 
 ggplot(df, aes(x = CSVTime, y = TableTime)) +
