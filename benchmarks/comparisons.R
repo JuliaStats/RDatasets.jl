75d5dcf59d955d08defa50064c9ababe33de7c8b
John Myles White <jmw@johnmyleswhite.com>
2012-11-27 09:07:09 -0500

    Added benchmarking tool

---
+++ benchmarks/comparisons.R
@@ -0,0 +1,19 @@
+df <- read.csv(file.path("results", "performance.tsv"), sep = "\t")
+
+ggplot(melt(df), aes(x = variable, y = value, fill = variable)) +
+  stat_summary(fun.data = "mean_cl_boot", geom = "bar") +
+  stat_summary(fun.data = "mean_cl_boot", geom = "errorbar") +
+  coord_flip() +
+  theme(legend.position = "none")
+
+ggplot(df, aes(x = CSVTime, y = TableTime)) +
+ geom_point() +
+ geom_abline() +
+ xlim(0, 8) +
+ ylim(0, 8)
+
+ggplot(df, aes(x = RCSVTime, y = TableTime)) +
+ geom_point() +
+ geom_abline() +
+ xlim(0, 8) +
+ ylim(0, 8)
---
+++ benchmarks/profile.jl
@@ -0,0 +1,57 @@
+# Load packages
+using Base
+
+load("DataFrames")
+using DataFrames
+
+load("RDatasets")
+
+# Test on all of the datasets from RDatasets
+datasets = RDatasets.datasets()
+
+# Keep track of the performance of read_table as TableTime and
+#  of csvDataFrame as CSVTime
+performance = DataFrame(length(datasets),
+                        ["Filename", "TableTime", "CSVTime"],
+                        {ASCIIString, Float64, Float64})
+
+# Compute R's performance on the same data sets
+run(`Rscript read_datasets.R`)
+
+# Force compilation of both reader functions while pulling
+#  in R performance data
+r_performance = read_table(file_path("results", "R_performance.csv"))
+r_performance = csvDataFrame(file_path("results", "R_performance.csv"))
+
+# Keep a record of the location of all data sets
+require("pkg")
+package_directory = file_path(Pkg.package_directory("RDatasets"), "data")
+
+# Loop over datasets and store performance in a Julian DataFrame
+i = 0
+for dataset in datasets
+  i += 1
+
+  directory, dataname = dataset[1], dataset[2]
+  filename = file_path(package_directory, directory, strcat(dataname, ".csv"))
+  performance[i, "Filename"] = filename
+
+  # Try out the different readers
+  try
+    performance[i, "TableTime"] = @elapsed read_table(filename)
+  end
+  try
+    performance[i, "CSVTime"] = @elapsed csvDataFrame(filename)
+  end
+end
+
+# Merge in R's performance information
+performance["RCSVTime"] = r_performance["RCSVTime"]
+
+# Write statistics to disk for visualization in R
+DataFrames.write_table(performance, file_path("results", "performance.tsv"))
+
+# How often do the readers fail to read a dataset?
+length(find(isna(performance["TableTime"])))
+length(find(isna(performance["CSVTime"])))
+length(find(isna(performance["RCSVTime"])))
