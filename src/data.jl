1ebe03c4215e2d4d58cd1ffdf58b0d20a645e823
John Myles White <jmw@johnmyleswhite.com>
2012-11-28 11:45:49 -0500

    Better error message for non-existent data sets

--- src/data.jl
+++ src/data.jl
@@ -8,5 +8,8 @@ function data{S <: String, T <: String}(package_name::S, dataset_name::T)
                        "data",
                        package_name,
                        strcat(dataset_name, ".csv"))
+  if !isfile(filename)
+    error("Unable to locate file $filename")
+  end
   return read_table(filename)
 end
