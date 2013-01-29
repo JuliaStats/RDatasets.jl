dcc46b6deee8da15c811f934416f194f9e4a6bfe
Douglas Bates <dmbates@gmail.com>
2013-01-29 15:04:05 -0600

    Use .rda format files when possible
    At present an .rda format file from the R source package will be used
    when it is gzip'd (as opposed to bzip2 or XZ compression) and contains
    exactly one data.frame object and that object does not have
    non-trivial row names.

--- src/data.jl
+++ src/data.jl
@@ -1,11 +1,15 @@
 function data{S <: String, T <: String}(package_name::S, dataset_name::T)
   package_directory = Pkg.dir("RDatasets")
-  filename = joinpath(package_directory,
-                       "data",
-                       package_name,
-                       strcat(dataset_name, ".csv"))
+  basename = joinpath(package_directory,
+                      "data",
+                      package_name)
+  rdaname = joinpath(basename, string(dataset_name, ".rda"))
+  if isfile(rdaname)
+    return DataFrame(read_rda(rdaname)[dataset_name])
+  end
+  filename = joinpath(basename, string(dataset_name, ".txt"))
   if !isfile(filename)
-    error("Unable to locate file $filename")
+    error("Unable to locate file $rdaname or $filename")
   end
   return read_table(filename)
 end
