7be4d66408ea20d6bb297215a82b115a4d9ec815
Sean Garborg <sean.garborg@gmail.com>
2015-04-09 12:10:55 -0600

    Consistent column names

--- test/dataset.jl
+++ test/dataset.jl
@@ -2,10 +2,7 @@ module TestDataset
     using Base.Test
     using RDatasets
 
-    function badname(sym)
-        n = string(sym)
-        ismatch(r"^[a-z]", n) || contains(n, ".")
-    end
+    goodname(sym) = ismatch(r"^([_xA-Z]|pH|pK)", string(sym))
 
     package_directory = joinpath(dirname(@__FILE__), "..", "data")
 
@@ -20,6 +17,18 @@ module TestDataset
         if size(df) != recorded_size
             println("Package: $pkg Dataset: $ds -")
             @test size(df) == recorded_size
+        end
+
+        if !all(map(goodname, names(df)))
+
+            println("Package: $pkg Dataset: $ds -")
+            println("Names: ", names(df))
+            throw(ErrorException("""
+
+                Column names should be match what 'readtable' would produce,
+                except words should be 'UpperCamelCase' (acronyms uppercase), &
+                '_' used only where two acronyms or two numbers would be concatenated.
+            """))
         end
     end
 end
