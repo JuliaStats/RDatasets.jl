655a8ebcee4dcc514adc67b4f7b65788f72a72a3
Alexey Stukalov <astukalov@gmail.com>
2017-09-21 13:09:04 +0000

    add testset layer for all datasets in a package
    so that test results for non-problematic packages are collapsed into
    a single row in the report

--- test/dataset.jl
+++ test/dataset.jl
@@ -1,19 +1,23 @@
 @testset "Datasets validity" begin
     goodname(sym) = ismatch(r"^([_xA-Z]|pH|pK)", string(sym))
 
-    sets = RDatasets.datasets()
-    for r in eachrow(sets)
-        pkg = convert(String, r[:Package])
-        ds = convert(String, r[:Dataset])
-        @testset "$pkg::$ds dataset" begin
-            recorded_size = (r[:Rows], r[:Columns])
-            df = dataset(pkg, ds)
+    allsets = RDatasets.datasets()
+    for pkgsets in groupby(allsets, :Package)
+        pkg = convert(String, pkgsets[1, :Package])
+        @testset "$pkg datasets" begin
+            for r in eachrow(pkgsets)
+                ds = convert(String, r[:Dataset])
+                @testset "$pkg::$ds dataset" begin
+                    recorded_size = (r[:Rows], r[:Columns])
+                    df = dataset(pkg, ds)
 
-            @test size(df) == recorded_size
-            # test there are no bad column names:
-            #  - words are 'UpperCamelCase' (acronyms uppercase)
-            #  - '_' used only where two acronyms or two numbers would be concatenated
-            @test names(df)[.!goodname.(names(df))] == String[]
+                    @test size(df) == recorded_size
+                    # test there are no bad column names:
+                    #  - words are 'UpperCamelCase' (acronyms uppercase)
+                    #  - '_' used only where two acronyms or two numbers would be concatenated
+                    @test names(df)[.!goodname.(names(df))] == String[]
+                end
+            end
         end
     end
 end
