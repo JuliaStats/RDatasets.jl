18c5a33378847f1c207b7476464ff2f4ea5634ad
Alexey Stukalov <astukalov@gmail.com>
2017-11-27 17:42:47 +0100

    minimize rows used to detect column types
    use 20 rows by default and define Dataset_typedetect_rows dictionary
    that stores the exceptions

--- src/dataset.jl
+++ src/dataset.jl
@@ -1,3 +1,18 @@
+# special cases for datasets that need more rows for proper column type detection
+const Dataset_typedetect_rows = Dict{Tuple{String, String}, Int}(
+    ("adehabitatLT", "albatross") => 1000,
+    ("adehabitatLT", "bear") => 1200,
+    ("adehabitatLT", "buffalo") => 1500,
+    ("adehabitatLT", "capreotf") => 600,
+    ("boot", "neuro") => 500,
+    ("gap", "PD") => 500,
+    ("plyr", "baseball") => 1000,
+    ("psych", "bfi") => 1000,
+    ("survival", "cancer") => 250,
+    ("survival", "lung") => 250,
+    ("vcd", "Bundesliga") => 15000,
+)
+
 function dataset(package_name::AbstractString, dataset_name::AbstractString)
     basename = joinpath(dirname(@__FILE__), "..", "data", package_name)
 
@@ -10,7 +25,7 @@ function dataset(package_name::AbstractString, dataset_name::AbstractString)
     if isfile(csvname)
         return open(GzipDecompressorStream, csvname, "r") do io
             CSV.read(io, delim=',', quotechar='\"', null="NA",
-                     rows_for_type_detect=15000)
+                     rows_for_type_detect=get(Dataset_typedetect_rows, (package_name, dataset_name), 200))
         end
     end
     error(@sprintf "Unable to locate dataset file %s or %s" rdaname csvname)
