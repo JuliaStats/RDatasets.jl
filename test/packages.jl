12ba809997bc4372611079fa21fbcfae10b8ea47
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-07-24 21:05:38 +0530

    Update packages.jl

--- test/packages.jl
+++ test/packages.jl
@@ -2,7 +2,7 @@
     ds = RDatasets.datasets()
     dp = RDatasets.packages()
 
-    @test dp[:Package] == sort(unique(ds[:Package]))
+    @test dp.Package == sort(unique(ds.Package))
 
     # test caching works
     @test dp === RDatasets.packages()
