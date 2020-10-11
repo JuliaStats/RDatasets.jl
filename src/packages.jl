6fcf85178c16410cba324e2555726c3d5460244a
Peter <46059512+petershintech@users.noreply.github.com>
2020-10-11 20:23:41 +1100

    Replace deprecated CSV.read() (#107)

--- src/packages.jl
+++ src/packages.jl
@@ -1,7 +1,7 @@
 function packages()
     if RDatasets.__packages === nothing
         path = joinpath(dirname(@__FILE__), "..", "doc", "packages.csv")
-        global __packages = CSV.read(path)
+        global __packages = DataFrame(CSV.File(path))
     end
     return RDatasets.__packages::DataFrame
 end
